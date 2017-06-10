4c8d8d7424746036d839e852f0f733f07703c349
an4morph <avgustin456@gmail.com>
2017-06-10 10:26:46 +0600

    sladkiy

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import searchStore from '../Stores/Search';
 import Header from './Header';
+import Page from './Page';
 import { observer } from 'mobx-react';
 
 import '../App.css';
@@ -12,6 +13,7 @@ class Main extends Component {
         return (
             <div className={className}>
                 <Header/>
+                <Page/>
             </div>
         );
     }
--- src/Components/Page.js
+++ src/Components/Page.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
 
 class Page extends Component {
     render() {
         return (
-            <div className="container">
-
+            <div className="news-container">
+            	<div className="newsColumn">
+            		<h2>TJournal</h2>
+            		<h2 className="secondHeader">Навальный</h2>
+            		<div className="newsItem">
+            			
+            			<div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
+            			<div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
+            			<span className="arrow-down"></span>
+            		</div>
+            		<h2 className="secondHeader">Навальный</h2>
+            		<div className="newsItem">
+            			
+            			<div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
+            			<div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
+            			<span className="arrow-down"></span>
+            		</div>
+            	</div>
+            	<div className="newsColumn">
+            		<h2>TJournal</h2>
+            		<h2 className="secondHeader">Навальный</h2>
+            		<div className="newsItem">
+            			
+            			<div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
+            			<div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
+            			<span className="arrow-down"></span>
+            		</div>
+            	</div>
             </div>
         );
     }
