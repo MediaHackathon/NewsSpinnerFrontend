120afaae595320aa31154d92f14be860eb54dd77
Roman Hinex <roman@hinex.org>
2017-06-11 04:23:05 +0600

    Update article list

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import ArticlesList from './ArticlesList';
+import searchStore from '../Stores/Search';
 import '../App.css';
 
-class Page extends Component {
+class Articles extends Component {
     render() {
         return (
             <div className="news-container">
-				<ArticlesList title="CNN"/>
-				<ArticlesList title="Kloop"/>
-				<ArticlesList title="Lenta.ru"/>
-				<ArticlesList title="Zanoza"/>
+				<ArticlesList title="CNN" data={searchStore.searchResult.cnn} />
+				<ArticlesList title="Kloop" data={searchStore.searchResult.kloop} />
+				<ArticlesList title="Lenta.ru" data={searchStore.searchResult.lenta} />
+				<ArticlesList title="Zanoza" data={searchStore.searchResult.zanoza} />
             </div>
         );
     }
 }
 
-export default Page;
+export default Articles;
--- src/Components/ArticlesList.js
+++ src/Components/ArticlesList.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
 import '../App.css';
 
+@observer
 class ArticlesList extends Component {
-    render() {
+
+    constructor(props) {
+        super(props);
+    }
+
+    article(data) {
+        console.log(data);
         return (
-            <div className="newsColumn">
-                <h2>TJournal</h2>
-                <h2 className="secondHeader">Навальный</h2>
+            <div>
+                <h2 className="secondHeader">{data.title}</h2>
                 <div className="newsItem">
 
                     <div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
                     <div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
                     <span className="arrow-down"></span>
                 </div>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="newsColumn">
+                <h2>{this.props.title}</h2>
+
+                {this.article({ title: 'asdasdasd' })}
             </div>
         );
     }
