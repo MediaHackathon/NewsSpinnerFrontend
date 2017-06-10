ba4551dfc179f1cfec11d62d0c3e4db698b5a966
an4morph <avgustin456@gmail.com>
2017-06-11 04:45:46 +0600

    filters.js

--- src/Components/ArticlesList.js
+++ src/Components/ArticlesList.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import { observer } from 'mobx-react';
 import '../App.css';
 
+
+@observer
 class ArticlesList extends Component {
 
     constructor(props) {
         super(props);
     }
 
-    article(data, index) {
+    article(data) {
+        console.log(data);
         return (
-            <div key={index}>
-                <h2 className="secondHeader">{data.title}</h2>
-                <div className="newsItem">
+            <div className="article">
+                <h2 className="articleHeader">{data.title}</h2>
+                <div className="articleItem">
 
                     <div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
                     <div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
@@ -22,16 +26,13 @@ class ArticlesList extends Component {
     }
 
     render() {
-        if (this.props.data.length === 0) {
-            return <div/>
-        }
-
         return (
             <div className="newsColumn">
                 <h2>{this.props.title}</h2>
 
-                {this.props.data.map(this.article)}
+                {this.article({ title: 'asdasdasd' })}
             </div>
+
         );
     }
 }
---
+++ src/Components/Filters.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import '../App.css';
+
+@observer
+class Filters extends Component {
+
+
+    render() {
+        return (
+            <div className="filterColumn">
+               filter
+            </div>
+        );
+    }
+}
+
+export default Filters;
