0d46925845627e7fc71cb3d008148d6bcf9bbd6e
Roman Hinex <roman@hinex.org>
2017-06-11 04:00:59 +0600

    Add articles list

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -1,38 +1,15 @@
 import React, { Component } from 'react';
+import ArticlesList from './ArticlesList';
 import '../App.css';
 
 class Page extends Component {
     render() {
         return (
             <div className="news-container">
-            	<div className="newsColumn">
-            		<h2>TJournal</h2>
-            		<h2 className="secondHeader">Навальный</h2>
-            		<div className="newsItem">
-            			
-            			<div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
-            			<div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
-            			<span className="arrow-down"></span>
-            		</div>
-            		<h2 className="secondHeader">Навальный</h2>
-            		<div className="newsItem">
-            			
-            			<div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
-            			<div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
-            			<span className="arrow-down"></span>
-            		</div>
-            	</div>
-            	<div className="newsColumn">
-            		<h2>TJournal</h2>
-            		<h2 className="secondHeader">Навальный</h2>
-            		<div className="newsItem">
-            			
-            			<div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
-            			<div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
-            			<span className="arrow-down"></span>
-            		</div>
-            	</div>
-
+				<ArticlesList title="CNN"/>
+				<ArticlesList title="Kloop"/>
+				<ArticlesList title="Lenta.ru"/>
+				<ArticlesList title="Zanoza"/>
             </div>
         );
     }
---
+++ src/Components/ArticlesList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class ArticlesList extends Component {
+    render() {
+        return (
+            <div className="newsColumn">
+                <h2>TJournal</h2>
+                <h2 className="secondHeader">Навальный</h2>
+                <div className="newsItem">
+
+                    <div className="picture"><img src="../img/facebook.png" width="81px" height="81px"/></div>
+                    <div className="text">fhjds fkjdk gkfjd gfdjg gkrjgerj rejior rjgggjdfkljgdf fkdjglkrej serkljg lsrek</div>
+                    <span className="arrow-down"></span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ArticlesList;
