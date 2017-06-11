e2c766566e7dc45c545c08541d33c213e2723f49
an4morph <avgustin456@gmail.com>
2017-06-11 09:23:23 +0600

    add slider

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -4,16 +4,18 @@ import searchStore from '../Stores/Search';
 import { observer } from 'mobx-react';
 import '../App.css';
 
+import { Carousel } from 'react-responsive-carousel';
+
 @observer
 class Articles extends Component {
     render() {
         return (
-            <div className="news-container">
-				<ArticlesList title="CNN" data={searchStore.searchResult.cnn} />
-				<ArticlesList title="Kloop" data={searchStore.searchResult.kloop} />
-				<ArticlesList title="Lenta.ru" data={searchStore.searchResult.lenta} />
-				<ArticlesList title="Zanoza" data={searchStore.searchResult.zanoza} />
-            </div>
+            <Carousel showArrows={true} className="news-container">
+				<div><ArticlesList title="CNN" data={searchStore.searchResult.cnn} /></div>
+				<div><ArticlesList title="Kloop" data={searchStore.searchResult.kloop} /></div>
+				<div><ArticlesList title="Lenta.ru" data={searchStore.searchResult.lenta} /></div>
+				<div><ArticlesList title="Zanoza" data={searchStore.searchResult.zanoza} /></div>
+            </Carousel>
         );
     }
 }
